9323a3b13652bbac245ac32786acd2aed6872d21
Mariano Videla <mvidela@gmail.com>
2010-07-13 10:18:37 -0300

    Fix on the Gin Injector

--- src/main/java/com/v2t/client/module/ClientModule.java
+++ src/main/java/com/v2t/client/module/ClientModule.java
@@ -2,13 +2,13 @@ package com.v2t.client.module;
 
 import com.google.gwt.inject.client.AbstractGinModule;
 import com.google.inject.Singleton;
-import com.v2t.client.GreetingServiceAsync;
+import com.v2t.client.ServerDispatchAsync;
 
 public class ClientModule extends AbstractGinModule{
 
 	@Override
 	protected void configure() {
-		bind( GreetingServiceAsync.class ).in( Singleton.class );
+		bind( ServerDispatchAsync.class ).in( Singleton.class );
 	}
 	
 }
